bfb4d5934211a136c759d109f8b249cb5d29d7f5
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-12-16 12:21:39 +0530

    basic email funcitons done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express =  require('express');
+const emailRoutes = require('./routes/email.routes')
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api', emailRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
     });
+
 
 
 app.listen(PORT, () => {
